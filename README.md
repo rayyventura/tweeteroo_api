388a6488d752ac28f9a98aa881cecbad45173518
Rayane Batista Ventura <rayyventura@gmail.com>
2022-01-24 21:42:20 -0300

    Fixes bug with tweet order

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tweteroo
+
+A back-end aplication utilizing the express library to simplify the HTTP comunication client-server
+
+
+## How to Run
+
+First, clone the repository or download the ZIP, then execute on the terminal the following steps
+</br>
+`npm i`
+ </br>
+`npm run start`
+
+## About
+
+An application to create a profile and utilize this simplified Twitter re-reading.
---
+++ index.js
@@ -0,0 +1,45 @@
+ import  express  from 'express'
+ import cors from 'cors'
+
+
+ let user = {
+     username: '',
+     avatar: '',
+     tweet: ''
+ }
+
+ let tweets = []
+
+ const server = express();
+
+ server.use(cors())
+ server.use(express.json())
+
+ server.post('/sign-up', (req, res) => {
+     const { username, avatar } = req.bod
+     user.username = username
+     user.avatar = avatar
+
+     res.send('OK')
+ })
+
+ server.post('/tweets', (req, res) => {
+     const { tweet } = req.body
+
+  tweets.push({ id, username: user.username, tweet, avatar: user.avatar })
+
+     
+     res.send('OK')
+ })
+
+ server.get('/tweets', (_req, res) => {
+    const tweetInverso = [...tweets].reverse().slice(0,10);
+        
+    res.send(tweetInverso);
+        
+     
+ })
+
+ server.listen(5000, () => {
+     console.log('Initiated Server...')
+ })
